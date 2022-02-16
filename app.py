08f9a70a571e794331f94bdc379e9b8f93e057bb
root <root@tokiman.xyz>
2022-02-16 14:20:49 -0500

    Add DB connect, template/static dirs for React build

--- app.py
+++ app.py
@@ -1,12 +1,21 @@
-from flask import Flask
+from flask import (Flask, render_template)
+import mariadb
 
 app = Flask(__name__)
 
+conn = mariadb.connect(
+    host='127.0.0.1',
+    port=3306,
+    user='cs340',
+    password='collector',
+    database='cartridge_collector'
+)
+cur = conn.cursor()
+
 @app.route("/")
-def root():
-    return "<p>Hello, Cartridge Collector!</p>"
+def index_page():
+    return render_template("index.html")
 
 
 if __name__ == "__main__":
     app.run(port='9191', host='0.0.0.0')
-    
