d6f1cc704d3b88d657ac9ccf58222a44fccf146f
mannijac <mannijac@oregonstate.edu>
2022-03-13 20:44:06 -0400

    syntax

--- DBModel.py
+++ DBModel.py
@@ -109,7 +109,7 @@ class DBModel():
             return {'error':'no rows updated'}
         return {'success':'{} rows have been updated'.format(str(len(res)))}
 
-    def delete(self, table_name->str, filter->str):
+    def delete(self, table_name, filter):
         if filter is None:     
             raise DBModelException()
         else:
