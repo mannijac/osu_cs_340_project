bf89d0afcf65742966e38e19a5c468b9e7b84557
mannijac <mannijac@oregonstate.edu>
2022-03-16 13:54:14 -0400

    json request

--- app.py
+++ app.py
@@ -37,10 +37,11 @@ def handle_api_call():
         return jsonify(db_model.update(table_name, table_attributes.keys(), table_attributes.values()))
     elif request.method == 'DELETE':
         # Delete entry
-        if request.args.get('table_name') is None:
+        print(table_attributes[])
+        if request.json.get('table_name') is None:
             return jsonify({'error': 'table_name is none!' }) 
 
-        table_name = request.args.get('table_name')
+        table_name = request.json['table_name']
         
         filter = {}
         print(table_name)
