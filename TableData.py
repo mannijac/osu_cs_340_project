ae8e7fbbc450a747d09ca6d29ae117739f31cb37
mannijac <mannijac@oregonstate.edu>
2022-03-17 19:36:03 -0400

    fix primary key lookup

--- TableData.py
+++ TableData.py
@@ -1,9 +1,9 @@
 table_data = {
     'users': {'primary_key': ['user_id']},
     'games': {'primary_key': ['game_id']},
-    'collections': {'primary_key': ['user_id', 'game_id']},
-    'wishes':  {'primary_key': ['user_id', 'game_id']},
-    'ratings':  {'primary_key': ['user_id', 'game_id']}    
+    'collections': {'primary_key': ['collection_id']},
+    'wishes':  {'primary_key': ['wish_id']},
+    'ratings':  {'primary_key': ['ratings_id']}    
 }
     
     
